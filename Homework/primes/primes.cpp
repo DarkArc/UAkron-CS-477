2b3445a1ad6ff3e184dab5456cc997f9741796ad
Wyatt Childers <wchilders@nearce.com>
2015-09-05 17:18:15 -0400

    Use spaces instead of tabs

--- Homework/primes/primes.cpp
+++ Homework/primes/primes.cpp
@@ -9,23 +9,23 @@
 
 bool is_prime(int n)
 {
-	auto j = static_cast<int>(sqrt(n));
-	for (int i = 2; i <= j; i++)
-	{
-		if (n % i == 0) return false;
-	}
-	return true;
+  auto j = static_cast<int>(sqrt(n));
+  for (int i = 2; i <= j; i++)
+  {
+    if (n % i == 0) return false;
+  }
+  return true;
 }
 
 
 int main()
 {
-	for (int i = 2; i < 25; i++)
-	{
-		if (is_prime(i))
-		{
-			std::cout << i << std::endl;
-		}
-	}
+  for (int i = 2; i < 25; i++)
+  {
+    if (is_prime(i))
+    {
+      std::cout << i << std::endl;
+    }
+  }
     return 0;
 }
