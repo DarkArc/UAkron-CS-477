5cefcc2f24be96944f6d910d8ebd6b954f1e6364
Wyatt Childers <wchilders@nearce.com>
2015-09-05 18:35:36 -0400

    Added parallelized primes.cpp

--- Homework/primes/primes.cpp
+++ Homework/primes/primes.cpp
@@ -3,9 +3,9 @@
 
 #include "stdafx.h"
 
+#include <vector>
 #include <iostream>
 #include "../../include/thread.h"
-
 
 bool is_prime(int n)
 {
@@ -20,12 +20,44 @@ bool is_prime(int n)
 
 int main()
 {
-  for (int i = 2; i < 25; i++)
-  {
-    if (is_prime(i))
+  const int threadCount = 10000;
+  const int elements = 1000000;
+  const int threadUnit = elements / threadCount;
+
+  std::vector<thread> threads;
+
+  try {
+    for (int i = 0; i < threadCount; ++i) {
+      threads.push_back(
+        create_thread([i, threadUnit]{
+          for (int k = i * threadUnit; k < (i + 1) * threadUnit; ++k) {
+              if (is_prime(k))
+              {
+                printf("%d\n", k);
+              }
+            }
+          }
+        )
+      );
+    }
+
+    for (auto&& thread : threads)
     {
-      std::cout << i << std::endl;
+      join(thread);
     }
   }
-    return 0;
+  catch (std::system_error &ex)
+  {
+    printf("Error: %d (%s)\n", ex.code().value(), ex.what());
+  }
+  catch (std::exception &ex)
+  {
+    printf("Error: %s\n", ex.what());
+  }
+  catch (...)
+  {
+    printf("Error!\n");
+  }
+
+  return 0;
 }
